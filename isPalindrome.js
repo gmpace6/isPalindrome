b0b91a13fc00ea231316bf77ba172f49103e2c26
Garrett Pace <elderpace6@gmail.com>
2023-04-24 10:37:40 -0600

    tweakedBetter

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,9 +1,16 @@
 // function isPalindrome(word) {
 //     return word === word.split('').reverse().join('')
 // }
-const isPalindrome = (word) => word === word.split('').reverse().join('')
 
-console.log(isPalindrome('racecar'))
-console.log(isPalindrome('hello'))
+// const isPalindrome = (word) => word === word.split('').reverse().join('')
+
+// const isPalindrome = (word) => word === word.toLowerCase().split('').reverse().join('')
+
+// const isPalindrome = (word) => word.toLowerCase().replace(' ','') === word.toLowerCase().replace(' ','').split('').reverse().join('')
+
+const isPalindrome = (word) => word.toLowerCase().replaceAll(' ','') === word.toLowerCase().replaceAll(' ','').split('').reverse().join('')
+
+console.log(isPalindrome('Race car'))
+console.log(isPalindrome('So Many Dynamos'))
 console.log(isPalindrome('Racecar'))
 console.log(isPalindrome('deified'))
