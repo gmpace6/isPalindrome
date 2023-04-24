2ab73cabcb276885ddf1a6a7efd9e2b9d47fe63d
Garrett Pace <elderpace6@gmail.com>
2023-04-24 10:24:56 -0600

    streamlined

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,1 +1,9 @@
-// Write your code below
+// function isPalindrome(word) {
+//     return word === word.split('').reverse().join('')
+// }
+const isPalindrome = (word) => word === word.split('').reverse().join('')
+
+console.log(isPalindrome('racecar'))
+console.log(isPalindrome('hello'))
+console.log(isPalindrome('Racecar'))
+console.log(isPalindrome('deified'))
